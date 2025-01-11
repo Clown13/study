9b36f3a887f4c35ad90a4befbb7eac6eb738ec46
Sanha <jeff_sanha@hotmail.com>
2025-01-10 23:55:28 -0500

    This is to init the GIT for study/review of Java.

---
+++ oop/Inheritance.java
@@ -0,0 +1,28 @@
+package oop;
+
+public class Inheritance {
+
+    class Animal {
+        public void eat () {
+            System.out.println("This Animal eats food.");
+        }
+
+        public void sleep() {
+            System.out.println("This Animal Sleeps");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("THis is to study Inheritance");
+    }
+
+//    class Parent {
+//        //Fields and methods of the parent class
+//
+//    }
+//
+//    class Child extends Parent {
+//        // Fields and methods of the parent class.
+//    }
+
+}
