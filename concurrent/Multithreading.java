c30b52090406530a0279d26df417556709a9ecd5
Sanha <jeff_sanha@hotmail.com>
2025-01-22 08:45:48 -0500

    January 21st 2025 - Testing Basic Multithreading.

---
+++ concurrent/Multithreading.java
@@ -0,0 +1,21 @@
+package concurrent;
+// Multithreading allows a program to execute multiple tasks simultaneously by dividing them to a smaller unit called threads.
+// Benefits: Better performance(Parallel Execution), responsiveness, and concurrency (Helps handling multiple tasks at once).
+class SampleThread extends Thread {
+    public void run() {
+        for (int i =0; i <5; i++) {
+            System.out.println(Thread.currentThread().getName() + "is running" + i );
+        }
+    }
+}
+
+public class Multithreading{
+    public static void main(String[] args) {
+        SampleThread thread1 = new SampleThread();
+        SampleThread thread2 = new SampleThread();
+
+        thread1.start();
+        thread2.start();
+    }
+
+}
